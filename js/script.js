5cd5d339898f7698d393c9eb1a695297e4f47894
SSWFreelancer <97734977+SSWFreelancer@users.noreply.github.com>
2022-02-03 17:48:27 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
-
 $(document).ready(function () {
    $('.header__burger').click(function (event) {
       $('.header__burger, .menu').toggleClass('active');
+       $('body').toggleClass('lock');
    });
 });
 
-
-
+$(document).ready(function () {
+   $('.menu__link').click(function (event) {
+      $('body').removeClass('lock');
+       $('.header__burger, .menu').removeClass('active');
+   });
+});
 
